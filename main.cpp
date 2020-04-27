3143740822042ef3a9d916eab24838fba64309f0
Adam Dunia <62951676+PadrinoAndiamo@users.noreply.github.com>
2020-04-27 20:42:22 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int main <int argc,char *argv[]){
+        cout<<"proram oblicza pola figur płaskich"<<endl;
+        start:
+        cout<<"\nCo chcesz policzyć?"
+            <<"\n\t1 - pole kola"
+            <<"\n\t2 - pole prostokonta"
+            <<"n\t3 - Pole trojkonta"<<endl;
+        int wybor;
+        cout<<"\nTwoj wybor:";
+        cin>>wybor;
+        float a, b ,wynik ,;
+
+    return 0;
+}
